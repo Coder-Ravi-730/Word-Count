f864c890dfb55c1a10b9a09c4f0597f0fea9ff7b
Ravi <coderravi730@gmail.com>
2022-10-23 19:01:07 +0530

    Dark Mode Enabled  Feature has been implemented

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import React , {useState} from "react";
 function App() {
+  const [mode, setMode] = useState('light') // Tells whether the dark mode is enabled or not
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#212529';
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3 ">
-        <TextForm heading = "Enter the text to analyze"/>
+        <TextForm heading="Enter the text to analyze" mode={mode} />
       </div>
     </>
   );
