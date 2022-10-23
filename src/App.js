85793d5bd1c7cec3cb06e4c8f7597a5e4ae6a5d8
Ravi <coderravi730@gmail.com>
2022-10-24 02:49:32 +0530

    Pre Final Commit without Router

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import About from "./Components/About";
+import React,{useState} from "react";
 import Alert from "./Components/Alert";
-
-import React , {useState} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 function App() {
   const [mode, setMode] = useState('light') // Tells whether the dark mode is enabled or not
   const [alert, setAlert] = useState(null)
@@ -16,28 +22,39 @@ function App() {
     })
     setTimeout(() => {
       setAlert(null);
-    },2000)
+    },3000)
   }
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
       document.body.style.backgroundColor = '   #150050';
       showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils- Dark Mode "
     } else {
       setMode('light')
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success")
-
+      document.title = "TextUtils- Light Mode "
     }
   }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
-      <div className="container my-3 ">
-        <TextForm showAlert = {showAlert} heading="Enter the text to analyze" mode={mode} />
+      <div className="container my-3">
+        <Switch>
+             <Route path="/about">
+              <About mode = {mode} />
+            </Route>
+            <Route path="/">
+             <TextForm showAlert={showAlert} heading="TextUtils- Word Counter,  Character Counter, to UpperCase, to LowerCase" mode={mode} />
+            </Route>
+        </Switch>
       </div>
+      </Router>
+
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -29,27 +29,27 @@ export default function TextForm(props) {
   return (
     <>
       <div className="container" style={{color:props.mode==='dark' ? 'white' : 'black'}}>
-        <h1>{props.heading}</h1>
+        <h3>{props.heading}</h3>
         <div className="mb-3">
           <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#98A8F8' : 'white', color: props.mode === 'dark'?'white':'black' }}
             id="Mybox"
-            rows="8"
+            rows="6"
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>
-          Convert to UpperCase!
-        </button>
-        <button className="btn btn-primary mx-2" onClick={handleDownClick}>
-          Convert to LowerCase!
-        </button>
-        <button className="btn btn-primary mx-2" onClick={clearText}>
-          Clear Text!
-        </button>
+            <button disabled = {text.length === 0} className="btn btn-primary" onClick={handleUpClick}>
+            Convert to UpperCase!
+            </button>
+            <button disabled = {text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handleDownClick}>
+            Convert to LowerCase!
+            </button>
+            <button disabled = {text.length === 0} className="btn btn-primary mx-2 my-2" onClick={clearText}>
+            Clear Text!
+            </button>
       </div>
       <div className="container my-3" style= {{color:props.mode==='dark'?'white':'black'}}>
-        <h1>Your Text summary</h1>
+        <h2>Your Text summary</h2>
         <p>
-          {text.split(" ").length} words and {text.length} Characters
+          {text.split(/\s+/).filter((element) =>{return element.length !==0}).length} words and {text.length} Characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h3>Preview</h3>
